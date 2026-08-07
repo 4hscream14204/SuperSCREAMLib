aa3c89d9fb18f0bbc50c60615a5b239c5ec2b6a8
PythonMaster245 <connorlithgow@4hscream.org>
2026-08-06 19:57:42 -0500

    Fixed package name and fixed ParallelRaceGroup

---
+++ core/src/main/java/org/screamrobotics/SuperSCREAMLib/hardware/HardwareDevice.java
@@ -0,0 +1,9 @@
+package org.screamrobotics.SuperSCREAMLib.hardware;
+
+public interface HardwareDevice {
+
+    void disable();
+
+    String getDeviceType();
+
+}
---
+++ core/src/main/java/org/screamrobotics/SuperSCREAMLib/util/Direction.java
@@ -0,0 +1,5 @@
+package org.screamrobotics.SuperSCREAMLib.util;
+
+public enum Direction {
+    LEFT, RIGHT, UP, DOWN, FORWARD, BACKWARDS
+}
